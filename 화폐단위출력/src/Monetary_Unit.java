db727889a44c7c869b398852ea840606b9eecae1
김보성 <kwunodong@gmail.com>
2020-12-24 23:55:12 +0900

    파일조정

---
+++ 화폐단위출력/src/Monetary_Unit.java
@@ -0,0 +1,39 @@
+//Monetary unit : 화폐단위 출력
+
+/* 출처 : 정보처리기사 실기시험
+ *
+ * 1.전체 알고리즘
+ * 1.1. 제목을 인쇄한다.
+ * 1.2. 화폐 매수를 저장할 배열을 초기화한다.
+ * 1.3. 이름과 출장비 지급액을 입력받는다.
+ * 1.4. 작업 완료 여부를 판단하여 마지막 사람의 출장비 지급액까지 화폐 단위별 매수를
+ *      계산이 끝났으면 소제목을 인쇄한 후 각 화폐별 전체 화폐 매수를 출력하고 종료한다.
+ * 1.5.
+ *
+ *
+ *
+ * */
+
+//================================================================================
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Monetary_Unit {
+    public static void main(String[] args) throws IOException {
+      //  SCanner inf = new Scanner(new FileReader("data.txt"));
+        int pm[] = new int[10];
+        int tm[] = new int[10];
+
+        String n;
+        int p, t, m, sw, k;
+
+        System.out.printf("\t\t\t출장비 지급표\n");
+        System.out.printf("성명 출장비지급액 오만원 만원 오천원 천원 오백원 백원 오십원 십원 오원 일원 \n");
+
+        System.out.printf("-----------------------------------------------------------\n");
+
+    }
+
+}
